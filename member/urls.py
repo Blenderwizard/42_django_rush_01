4402c9e1d40bd88151c94da92158934ed3b7f0db
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-06-12 13:46:10 +0200

    Profil public

--- member/urls.py
+++ member/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import RegisterFormView, LoginFormView, LogoutView, UpdateInformationView
+from .views import RegisterFormView, LoginFormView, LogoutView, UpdateInformationView, MemberDetailView
 
 urlpatterns = [
     path('', views.temp, name='home'),
     path('register/', RegisterFormView.as_view(), name='register'),
     path('login/', LoginFormView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-	path('user/<int:pk>', UpdateInformationView.as_view(), name='user info')
+	path('user/<int:pk>', MemberDetailView.as_view(), name='user'),
+	path('user/<int:pk>/edit', UpdateInformationView.as_view(), name='user info')
 ]
